184ec29e124da1ae0ae2951292ce329b16995502
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-08-13 23:15:54 -0500

    Router: Use a Switch to select which component to show

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import {Route, NavLink } from 'react-router-dom'; 
+import {Route, NavLink, Switch } from 'react-router-dom'; 
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -21,7 +22,7 @@ class Blog extends Component {
                                     activeStyle={{
                                         color: '#fa923f',
                                         textDecoration: 'underline'
-                                    }}>Home</NavLink></li>
+                                    }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
@@ -33,7 +34,10 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home</h1>} /> */}
                 <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
